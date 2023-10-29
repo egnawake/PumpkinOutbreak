569c6d14ae64ffbbaf61dbcf9be07b62aed66b64
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2023-10-29 03:27:58 +0000

    Fix bug with aranha and tree

--- Assets/Scripts/LevelChanger.cs
+++ Assets/Scripts/LevelChanger.cs
@@ -6,23 +6,9 @@ using UnityEngine.SceneManagement;
 
 public class LevelChanger : MonoBehaviour
 {
-    FMOD.Studio.EventInstance mainSong;
 
-    public LevelChanger(EventInstance mainSong)
+    private void OnTriggerEnter(Collider collider)
     {
-        this.mainSong = FMODUnity.RuntimeManager.CreateInstance("event:/Main");
-    }
-
-    void Start()
-    {
-        mainSong.start();
-        mainSong.release();
-    }
-
-    private void OnTriggerEnter(Collider player)
-    {
-        mainSong.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
-
         SceneManager.LoadScene("Village");
     }
 }
