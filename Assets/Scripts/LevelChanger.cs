8da9aa4fc31b5893a175fb2ad5942d0517d52f2f
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2023-10-29 02:17:19 +0000

    Add levelchanger fix character sprite flip

---
+++ Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,27 @@
+using FMOD.Studio;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+    FMOD.Studio.EventInstance mainSong;
+
+    public LevelChanger(EventInstance mainSong)
+    {
+        this.mainSong = FMODUnity.RuntimeManager.CreateInstance("event:/Main");
+    }
+
+    void Start()
+    {
+        mainSong.start();
+    }
+
+    private void OnTriggerEnter(Collider player)
+    {
+        mainSong.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+
+        SceneManager.LoadScene("Village");
+    }
+}
